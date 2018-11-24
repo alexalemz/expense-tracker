1b74893dfb13435402ebc102150665c7597c79f0
Alex Alemzadeh <alex.alemzadeh@gmail.com>
2018-11-24 14:20:04 -0800

    Add session storage with connect-session-sequelize

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 var exphbs = require("express-handlebars");
+
+// initalize sequelize with session store
+var SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 var db = require("./models");
 
@@ -12,11 +16,19 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(session({ 
+  secret: "keyboard cat", 
+  store: new SequelizeStore({
+    db: db.sequelize
+  }),
+  resave: false, 
+  saveUninitialized: false 
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
